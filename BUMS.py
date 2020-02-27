dce34861d3b155b6f51c0f166a88723600af6cf6
Young <Hunter.Young@kindred.com>
2020-02-27 15:55:38 -0500

    added logzero & logging

--- BUMS.py
+++ BUMS.py
@@ -10,6 +10,9 @@
 ##################################################
 
 #   imports
+import logzero
+from logzero import logger
+
 
 #   global variables
 report = int()
@@ -19,41 +22,58 @@ action = int()
 
 
 def ask(report, dir1, dir2, action):
-    print('### STARTING ASK ###')
+    logger.debug('### STARTING ASK ###')
     
     #   the REPORT
+    logger.debug('# STARTING REPORT #')
     print('What kind of report would you like?')
     reportAsk = input("Enter 1 for .TXT and 2 to print to the screen:\n")
-    report = int(reportAsk)
+    reportChoice = int(reportAsk)
+    report = str()
 
-    if report == 1:
+    if reportChoice == 1:
         print('You chose a TEXT report')
-    elif report == 2:
+        report = str('TEXT')
+    elif reportChoice == 2:
         print('you chose to print the report to the screen')
+        report = str('SCREEN')
     else:
         print('You did not select a proper choice. Terminating')
+        report = str('REPORT ERROR')
+
+    logger.info(report)
 
     #   the DIRECTORIES
+    logger.debug('# STARTING DIRECTORIES #')
     print('What directories would you like to scan?')    
     dir1Ask = input('Enter Directory 1:')
     dir1 = str(dir1Ask)
+    logger.info(dir1)
     dir2Ask = input('Enter Directory 2:')
     dir2 = str(dir2Ask)
     print('You chose\n {} as Directory 1\n and\n {} as Directory 2'.format(dir1, dir2))
+    logger.info(dir2)
 
-    #   the ASK
+    #   the ACTION
+    logger.debug('# STARTING ACTION #')
     print('What action would you like to perform?')
     actionAsk = input('Enter 1 for PUSH, 2 for PULL, and 3 for BOTH:')
-    action = int(actionAsk)
+    actionChoice = int(actionAsk)
+    action = str()
 
-    if action == 1:
+    if actionChoice == 1:
         print('You chose to PUSH data.')
-    elif action == 2:
+        action = str('PUSH')
+    elif actionChoice == 2:
         print('You chose to PULL data.')
-    elif action == 3:
+        action = str('PULL')
+    elif actionChoice == 3:
         print('You chose to PUSH & PULL data.')
+        action = str('BOTH')
     else:
         print('You did not select a proper choice. Terminating.')
+        action = str('ACTION ERROR')
+    logger.info(action)
     
     return report
     return dir1
@@ -63,9 +83,9 @@ def ask(report, dir1, dir2, action):
 
 
 def main():
-    print('### STARTING MAIN ###')
+    logger.debug('### STARTING MAIN ###')
     ask(report, dir1, dir2, action)
-    print('### BACK IN MAIN ###')
+    logger.debug('### ENDING MAIN ###')
 
 
 main()
