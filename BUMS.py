da394a75f431710a52c18e780beeda6319c26f8a
hunter <15759092+jhyoung09@users.noreply.github.com>
2020-03-04 21:20:02 -0500

    updated dir_compare function

--- BUMS.py
+++ BUMS.py
@@ -94,43 +94,38 @@ def date():
     date = datetime.datetime.now()
     return date.strftime('%d %m %Y')
 
-def dir_compare(dir1, dir2, prefix='.'):
-    """Return a recursive dircmp comparison report as a dictionary."""
+def dir_compare(dir1,dir2,prefix = '.'):
+    logger.debug('### STARTING DIR_COMPARE')
 
-    comparison = filecmp.dircmp(dir1, dir2)
+    logger.debug('# SET DCMP & DATA #')
+    dcmp = filecmp.dircmp(dir1,dir2)
 
     data = {
-        'left': [r'{}/{}'.format(prefix, i) for i in comparison.left_only],
-        'right': [r'{}/{}'.format(prefix, i) for i in comparison.right_only],
-        'both': [r'{}/{}'.format(prefix, i) for i in comparison.common_files],
+        'dir1': [r'{}/{}'.format(prefix, i) for i in dcmp.left_only],
+        'dir2': [r'{}/{}'.format(prefix, i) for i in dcmp.right_only],
+        'both': [r'{}/{}'.format(prefix, i) for i in dcmp.common_files],
     }
+
+    logger.debug('# SETTING DATALIST & SORTING DATALIST #')
 
     for datalist in data.values():
         datalist.sort()
 
-    if comparison.common_dirs:
-        for folder in comparison.common_dirs:
-            # Update prefix to include new sub_folder
+    if dcmp.common_dirs():
+        for folder in dcmp.common_dirs:
             prefix += '/' + folder
-
-            # Compare common folder and add results to the report
-            sub_folder1 = os.path.join(folder1, folder)
-            sub_folder2 = os.path.join(folder2, folder)
-            sub_report = _recursive_dircmp(sub_folder1, sub_folder2, prefix)
-
-            # Add results from sub_report to main report
+            
+            sub_dir1 = os.path.join(dir1, folder)
+            sub_dir2 = os.path.join(dir2, folder)
+            sub_dir_compare = dir_compare(sub_dir1,sub_dir2,prefix)
+            
             for key, value in sub_report.items():
                 data[key] += value
-
+        
     return data
-    list(data)
 
-
-
+    logger.info(data)
     
-
-
-
 
 
 def main():
@@ -143,7 +138,7 @@ def main():
 
     reporting(report, dir1, dir2, action)
 
-    dir_compare(dir1, dir2, prefix='.')
+    dir_compare(dir1,dir2,prefix = '.')
 
     logger.debug('### ENDING MAIN ###')
 
